aa07dc230097d0b3087286e34726d24fc3489b52
Topher <tophersymps@gmail.com>
2018-06-25 18:49:10 -0400

    Adding login form to landing page

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -12,7 +12,20 @@ import Footer from "components/Footer/Footer.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import Button from "components/CustomButtons/Button.jsx";
-import HeaderLinks from "components/Header/HeaderLinks.jsx";
+
+// Added imports for login card
+import InputAdornment from "@material-ui/core/styles/withStyles";
+import Email from "@material-ui/icons/Email";
+import LockOutline from "@material-ui/icons/LockOutline";
+import People from "@material-ui/icons/People";
+import Card from "components/Card/Card.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardFooter from "components/Card/CardFooter.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+//***************************************************************
+
+// import HeaderLinks from "components/Header/HeaderLinks.jsx";
 import Parallax from "components/Parallax/Parallax.jsx";
 
 import landingPageStyle from "assets/jss/material-kit-react/views/landingPage.jsx";
@@ -25,6 +38,26 @@ import TeamSection from "./Sections/TeamSection.jsx";
 const dashboardRoutes = [];
 
 class LandingPage extends React.Component {
+  
+  // Login code
+  constructor(props) {
+    super(props);
+    // we use this to make the card appear after the page has been rendered
+    this.state = {
+      cardAnimaton: "cardHidden"
+    };
+  }
+  componentDidMount() {
+    // we add a hidden class to the card and after 700 ms we delete it and the transition appears
+    setTimeout(
+      function() {
+        this.setState({ cardAnimaton: "" });
+      }.bind(this),
+      700
+    );
+  }
+  //********
+
   render() {
     const { classes, ...rest } = this.props;
     return (
@@ -44,11 +77,8 @@ class LandingPage extends React.Component {
         <Parallax filter image={require("assets/img/devScreen1.jpeg")}>
           <div className={classes.container}>
             <GridContainer>
-              <GridItem xs={12} sm={12} md={6}>
+              <GridItem xs={12} sm={12} md={8}>
                 <h1 className={classes.title}>The landing page for Devs.</h1>
-                {/* <h4>
-                  It's hard out there for us devs to keep everything together. Especially if you're just starting out and trying to stay organized. With so many outlets for information why not have a place where it can all be kept?! Welcome to Ren&lt;dev&gt;ous. A place to organize your progress and get intouch with people on the same path.
-                </h4> */}
                 <br />
                 <Button
                   color="success"
@@ -59,6 +89,99 @@ class LandingPage extends React.Component {
                 >
                   <i className="fas fa-play" />Get your Roll on!
                 </Button>
+              </GridItem>
+              <GridItem xs={12} sm={12} md={4}>
+                <Card className={classes[this.state.cardAnimaton]}>
+                  <form className={classes.form}>
+                    <CardHeader color="primary" className={classes.cardHeader}>
+                      <h4>Login</h4>
+                      <div className={classes.socialLine}>
+                        <Button
+                          justIcon
+                          href="#pablo"
+                          target="_blank"
+                          color="transparent"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className={"fab fa-twitter"} />
+                        </Button>
+                        <Button
+                          justIcon
+                          href="#pablo"
+                          target="_blank"
+                          color="transparent"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className={"fab fa-facebook"} />
+                        </Button>
+                        <Button
+                          justIcon
+                          href="#pablo"
+                          target="_blank"
+                          color="transparent"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className={"fab fa-google-plus-g"} />
+                        </Button>
+                      </div>
+                    </CardHeader>
+                    <p className={classes.divider}>Or Be Classical</p>
+                    <CardBody>
+                      <CustomInput
+                        labelText="First Name..."
+                        id="first"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "text",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <People className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      <CustomInput
+                        labelText="Email..."
+                        id="email"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "email",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Email className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      <CustomInput
+                        labelText="Password"
+                        id="pass"
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: "password",
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <LockOutline
+                                className={classes.inputIconsColor}
+                              />
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                    </CardBody>
+                    <CardFooter className={classes.cardFooter}>
+                      <Button simple color="primary" size="lg">
+                        Get started
+                      </Button>
+                    </CardFooter>
+                  </form>
+                </Card>
               </GridItem>
             </GridContainer>
           </div>
