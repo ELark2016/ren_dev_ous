e372d7950aa12a75643c8c34195bec0ee0f3f265
Topher <tophersymps@gmail.com>
2018-07-16 16:37:49 -0400

    Primary layout set.
    Continue to make it pretty..

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import classNames from "classnames";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import Hidden from "@material-ui/core/Hidden";
+import Drawer from "@material-ui/core/Drawer";
+// @material-ui/icons
+import Menu from "@material-ui/icons/Menu";
+// core components
+import headerStyle from "../../assets/jss/material-dashboard-react/components/headerStyle.jsx";
+
 const Header = () => (
   
   <header>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -7,6 +7,32 @@ import {
 import { Redirect } from 'react-router-dom'
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
+// UI Kit Magic
+import withStyles from "@material-ui/core/styles/withStyles";
+import classNames from "classnames";
+// @material-ui/icons
+import Email from "@material-ui/icons/Email";
+import LockOutline from "@material-ui/icons/LockOutline";
+import People from "@material-ui/icons/People";
+// UI Kit core components
+import GridContainer from "../../components/Grid/GridContainer.jsx";
+import GridItem from "../../components/Grid/GridItem.jsx";
+import Button from "../../components/CustomButtons/Button.jsx";
+// import HeaderLinks from "../../components/Header/HeaderLinks.jsx";
+import Parallax from "../../components/Parallax/Parallax.jsx";
+// UI Kit core components for Signup Form
+import Card from "../../components/Card/Card.jsx";
+import CardBody from "../../components/Card/CardBody.jsx";
+import CardHeader from "../../components/Card/CardHeader.jsx";
+import CardFooter from "../../components/Card/CardFooter.jsx";
+import CustomInput from "../../components/CustomInput/CustomInput.jsx";
+// form styles
+import loginPageStyle from "../../assets/jss/material-dashboard-react/layouts/loginPage.jsx";
+
+// import images
+import image from "../../assets/img/sidebar-5.jpg";
+
+import landingPageStyle from "../../assets/jss/material-dashboard-react/layouts/landingPage.jsx";
 
 class Home extends Component {
   constructor(props) {
@@ -153,18 +179,90 @@ class Home extends Component {
 
       <div>
         <Header />
-        <div className='container'>
+        <div
+          className="pageHeader"
+          style={{
+            backgroundImage: "url(" + image + ")",
+            backgroundSize: "cover",
+            backgroundPosition: "top center"
+          }}
+        >
+        {/* // ******* UI Kit Work ******* */}
+          <div className="container">
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={8}>
+                <br />
+                <h1 className="title white-text">Your Story Starts With Us.</h1>
+                <h4 className="white-text">
+                  Every landing page needs a small description after the big
+                  bold title, that's why we added this text here. Add here all
+                  the information that can make you or your product create the
+                  first impression.
+                </h4>
+                <br /> 
+              </GridItem>
+
+              {/* ************************************ */}
+              {/* FIXME: Update Sign up form to UI Kit */}
+              {/* ************************************ */}
+              <GridItem xs={12} sm={12} md={4}>
+                <div className="card grey darken-1">
+                  <div className="card-content white-text">
+                    {
+                      (signUpError) ? (
+                        <p>{signUpError}</p>
+                      ) : (null)
+                    }
+                    <span className="card-title">Sign Up!</span>
+                    <input
+                    type="text"
+                    placeholder="First Name"
+                    value={signUpFirstName}
+                    onChange={this.onTextBoxChangeSignUpFirstName} />
+                    <br /><br />
+                    <input
+                    type="text"
+                    placeholder="Last Name"
+                    value={signUpLastName}
+                    onChange={this.onTextBoxChangeSignUpLastName} />
+                    <br /><br />
+                    <input
+                    type="email"
+                    placeholder="Email"
+                    value={signUpEmail}
+                    onChange={this.onTextBoxChangeSignUpEmail} />
+                    <br /><br />
+                    <input
+                    type="password"
+                    placeholder="Password"
+                    value={signUpPassword}
+                    onChange={this.onTextBoxChangeSignUpPassword} />
+                    <br /><br />
+                    <button className='btn' color="#ff1744" onClick={this.onSignUp}>Sign Up
+                      <i class="material-icons right">send</i>
+                    </button>
+                  </div>
+                </div>
+              </GridItem>
+
+            </GridContainer>
+          </div>
+        </div>
+
+        {/* // ******* END UI Kit Work ******* */}
+
+        {/* <div className='container'>
           <div className="row">
             
-            <div className="col s6">
-              <div className="card blue-grey darken-1">
-                <div className="card-content white-text">
+            <div className="col s8 valign-center">
+              <div className="card">
+                <div className="card-content black-text">
                   <h1>Hello World!</h1>
                 </div>
               </div>
             </div>
             
-            <div className="col s6">
+            <div className="col s4">
               <div className="card blue-grey darken-1">
                 <div className="card-content white-text">
                   {
@@ -172,7 +270,7 @@ class Home extends Component {
                       <p>{signUpError}</p>
                     ) : (null)
                   }
-                  <span className="card-title">Sign Up Beaches!</span>
+                  <span className="card-title">Sign Up!</span>
                   <input
                   type="text"
                   placeholder="First Name"
@@ -201,9 +299,9 @@ class Home extends Component {
                 </div>
               </div>
             </div> 
-          </div> {/*End of row*/}
+          </div> 
 
-        </div>
+        </div> */}
         <Footer />
       </div>
 
@@ -217,4 +315,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default withStyles(landingPageStyle)(Home);
